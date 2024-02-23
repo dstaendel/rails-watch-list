1dc7f08bc5e19260a542b16756ac1c13db4b3629
Dominic Staendel <dominic.staendel@hotmail.de>
2024-02-23 12:35:23 +0100

    update

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,32 +1,33 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
   def new
-    # Initialize a new bookmark object for the form
     @bookmark = Bookmark.new
   end
 
   def create
-    # Create a new bookmark from form parameters
     @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:id])
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path(params[:id]), notice: 'Bookmark was successfully created.'
+      redirect_to list_path(@list), notice: 'Bookmark was successfully created.'
     else
       render :new
     end
   end
 
   def destroy
-    # Find the bookmark by ID and destroy it
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to bookmarks_path, notice: 'Bookmark was successfully destroyed.'
+    redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully deleted.'
   end
 
   private
 
-  # Strong parameters for bookmark
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:movie_id, :comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,17 +12,10 @@ Rails.application.routes.draw do
 
   # Route to create a new list
   post 'lists', to: 'lists#create'
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: 'new_list_bookmark'
 
-  get 'lists/:id/bookmarks/new', to: 'bookmarks#new', as: 'new_bookmark'
-
-  post 'bookmarks', to: 'bookmarks#create'
+  post '/lists/:list_id/bookmarks', to: 'bookmarks#create', as: "list_bookmarks"
 
   delete 'bookmarks/:id', to: 'bookmarks#destroy', as: 'bookmark'
-
 end
